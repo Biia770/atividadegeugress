b89d2e7f2f831088ac4bf008e2cc69f1ddc5cf0e
Biia770 <annabeatrizsr3011@gmail.com>
2024-11-07 11:12:51 -0300

    Add files via upload

--- ola.dart
+++ ola.dart
@@ -12,17 +12,12 @@ void main(){
   print("a subtracao do $num1 e $num2 e $subtracao");
   int multiplicacao = num1 * num2;
   print("a multiplicacao do $num1 x $num2 e $multiplicacao");
-  if (num2 == 0){
+    while(num2 == 0){
     print("Nao e possivel fazer a divisao, escolha outro numero:");
-    int num2 = int.parse(stdin.readLineSync()!);
-    double divis = num1 / num2;
-    print("a divisao do $num1 e $num2 e $divis");
+    num2 = int.parse(stdin.readLineSync()!);
+  
   } 
-  else{
-    double divisao = num1 / num2;
-    print("a divisao do $num1 e $num2 e $divisao");
-  }
+    double div = num1 / num2;
+    print("Divisao: $div");
   
-  
-
 }
